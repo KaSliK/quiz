502645fcdb0edfb548e5bef2b7ffaa840440bd26
Kamil Kaślikowski <kamil.kaslikowski@gmail.com>
2019-04-02 20:24:57 +0200

    All

---
+++ QUIZ/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace QUIZ
+{
+    partial class QUIZ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QUIZ));
+            this.leftPlus = new System.Windows.Forms.Label();
+            this.sign = new System.Windows.Forms.Label();
+            this.rightPlus = new System.Windows.Forms.Label();
+            this.equa = new System.Windows.Forms.Label();
+            this.startQuiz = new System.Windows.Forms.Button();
+            this.plusResult = new System.Windows.Forms.NumericUpDown();
+            this.check = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.plusResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftPlus
+            // 
+            resources.ApplyResources(this.leftPlus, "leftPlus");
+            this.leftPlus.Name = "leftPlus";
+            // 
+            // sign
+            // 
+            resources.ApplyResources(this.sign, "sign");
+            this.sign.Name = "sign";
+            // 
+            // rightPlus
+            // 
+            resources.ApplyResources(this.rightPlus, "rightPlus");
+            this.rightPlus.Name = "rightPlus";
+            // 
+            // equa
+            // 
+            resources.ApplyResources(this.equa, "equa");
+            this.equa.Name = "equa";
+            // 
+            // startQuiz
+            // 
+            resources.ApplyResources(this.startQuiz, "startQuiz");
+            this.startQuiz.Name = "startQuiz";
+            this.startQuiz.UseVisualStyleBackColor = true;
+            this.startQuiz.Click += new System.EventHandler(this.startQuiz_Click);
+            // 
+            // plusResult
+            // 
+            resources.ApplyResources(this.plusResult, "plusResult");
+            this.plusResult.Name = "plusResult";
+            this.plusResult.ValueChanged += new System.EventHandler(this.plusResult_ValueChanged);
+            // 
+            // check
+            // 
+            resources.ApplyResources(this.check, "check");
+            this.check.Name = "check";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeLabel
+            // 
+            resources.ApplyResources(this.TimeLabel, "TimeLabel");
+            this.TimeLabel.Name = "TimeLabel";
+            // 
+            // QUIZ
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.plusResult);
+            this.Controls.Add(this.startQuiz);
+            this.Controls.Add(this.equa);
+            this.Controls.Add(this.rightPlus);
+            this.Controls.Add(this.sign);
+            this.Controls.Add(this.leftPlus);
+            this.Name = "QUIZ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.plusResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label leftPlus;
+        private System.Windows.Forms.Label sign;
+        private System.Windows.Forms.Label rightPlus;
+        private System.Windows.Forms.Label equa;
+        private System.Windows.Forms.Button startQuiz;
+        private System.Windows.Forms.NumericUpDown plusResult;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TimeLabel;
+    }
+}
+
---
+++ QUIZ/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUIZ
+{
+    public partial class QUIZ : Form
+    {
+        int add1, add2, timeLeft, result = 0;
+        Random rand = new Random();
+        public QUIZ()
+        {
+            InitializeComponent();
+            check.Enabled = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            TimeLabel.Text = timeLeft + " sekund";
+            if (timeLeft == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Twoj wynik to " + result + " poprawnych odpowiedzi");
+                check.Enabled = false;
+                startQuiz.Enabled = true;
+            }
+        }
+        private void startQuiz_Click(object sender, EventArgs e)
+        {
+            TimeLabel.Text = "30";
+            timeLeft = 30;
+            timer1.Start();
+            start();
+        }
+        private void check_Click(object sender, EventArgs e)
+        {
+            
+            if (checkAnswer() == true)
+            {
+                check.Enabled = false;
+                MessageBox.Show("RACJA");
+                //startQuiz.Enabled = true;
+                start();
+                result++;
+
+            }
+
+            else MessageBox.Show("Próbuj dalej!!!");
+        }
+        private void start()
+        {
+            check.Enabled = true;
+            startQuiz.Enabled = false;
+            add1 = rand.Next(21);
+            add2 = rand.Next(21);
+            while(add2>add1)
+            {
+                add2 = rand.Next(21);
+            }
+            changeFlag();
+            leftPlus.Text = add1.ToString();
+            rightPlus.Text = add2.ToString();
+        }
+        private void changeFlag()
+        {
+            flaga++;
+            if(flaga==1)
+            {
+                sign.Text = "+";
+            }
+            else if(flaga==2)
+            {
+                sign.Text = "-";
+                
+            }
+            else if (flaga == 3)
+            {
+                sign.Text = "*";
+                
+            }
+            else if (flaga == 4)
+            {
+                sign.Text = "/";
+                flaga = 0;
+            }
+        }
+
+        private void plusResult_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        int flaga = 0;
+        private bool checkAnswer()
+        {
+            if (add1 + add2 == plusResult.Value && flaga == 1) return true;
+            else if (add1 - add2 == plusResult.Value && flaga == 2) return true;
+            else if (add1 * add2 == plusResult.Value && flaga == 3) return true;
+            else if (add1 / add2 == plusResult.Value && flaga == 0) return true;
+            else return false;
+        }
+    }
+}
